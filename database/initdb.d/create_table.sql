ebfcd7a8c06bbc4e01e683d4b35d3e64d546f646
JadeKim042386 <jk042386@gmail.com>
2024-03-04 15:28:16 +0900

    chore: DB에 article_file 테이블 추가 및 테스트 데이터 추가

--- database/initdb.d/create_table.sql
+++ database/initdb.d/create_table.sql
@@ -12,10 +12,22 @@ create table user_account(
     primary key (id)
 );
 
+create sequence seq_article_file start 1;
+create table article_file(
+    id bigint default nextval('seq_article_file') NOT NULL ,
+    byte_size bigint NOT NULL ,
+    original_file_name varchar(255) NOT NULL ,
+    file_name varchar(255) NOT NULL ,
+    file_extension varchar(255) NOT NULL ,
+    dimension_option jsonb,
+    primary key (id)
+);
+
 create sequence seq_article start 1;
 create table article(
     id bigint default nextval('seq_article') NOT NULL ,
     user_account_id bigint NOT NULL ,
+    article_file_id bigint ,
     title varchar(255) NOT NULL ,
     content varchar(255) NOT NULL ,
     article_type varchar(255) NOT NULL ,
@@ -24,7 +36,8 @@ create table article(
     created_at timestamp NOT NULL ,
     modified_at timestamp ,
     primary key (id),
-    foreign key (user_account_id) references user_account (id)
+    foreign key (user_account_id) references user_account (id),
+    foreign key (article_file_id) references article_file (id)
 );
 
 create sequence seq_article_comment start 1;
@@ -38,4 +51,4 @@ create table article_comment(
     primary key (id),
     foreign key (user_account_id) references user_account (id),
     foreign key (article_id) references article (id)
-)
+);
