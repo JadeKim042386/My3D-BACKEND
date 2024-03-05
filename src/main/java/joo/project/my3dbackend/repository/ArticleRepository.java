74aa6a1d2834c33ba6bcce6473f9dd871e85ed72
JadeKim042386 <jk042386@gmail.com>
2024-03-05 17:58:27 +0900

    feat: 게시글 목록 조회 API 구현 - 페이지당 9개씩 조회하며 생성일시 기준으로 내림차순 정렬 - userAccount와 join fetch  적용 #15

--- src/main/java/joo/project/my3dbackend/repository/ArticleRepository.java
+++ src/main/java/joo/project/my3dbackend/repository/ArticleRepository.java
@@ -1,12 +1,19 @@
 package joo.project.my3dbackend.repository;
 
 import joo.project.my3dbackend.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+import static org.springframework.data.jpa.repository.EntityGraph.EntityGraphType.LOAD;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @EntityGraph(attributePaths = "userAccount", type = LOAD)
+    Page<Article> findAll(Pageable pageable);
+
     @EntityGraph(attributePaths = {"userAccount", "articleComments"})
     Optional<Article> findFetchAllById(Long articleId);
 }
