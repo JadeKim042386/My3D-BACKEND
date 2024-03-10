50f32d51a5579dec8091f6b128eeabb2d3c6d78d
JadeKim042386 <jk042386@gmail.com>
2024-03-10 17:39:57 +0900

    feat: 부모 댓글 삭제시 자식 댓글도 삭제되도록 JPA 쿼리 수정 #40

--- src/main/java/joo/project/my3dbackend/repository/ArticleCommentRepository.java
+++ src/main/java/joo/project/my3dbackend/repository/ArticleCommentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -25,4 +26,12 @@ public interface ArticleCommentRepository extends JpaRepository<ArticleComment, 
             """)
     @EntityGraph(attributePaths = "userAccount", type = LOAD)
     Page<ArticleComment> findAll(Pageable pageable, Long parentCommentId);
+
+    /**
+     * 댓글 삭제
+     * - 부모 댓글을 삭제할 경우 자식 댓글의 parentId를 확인하여 같이 삭제
+     */
+    @Modifying
+    @Query("delete from ArticleComment ac where ac.id=?1 or ac.parentCommentId=?1")
+    void deleteByIdOrParentCommentId(Long articleCommentId);
 }
