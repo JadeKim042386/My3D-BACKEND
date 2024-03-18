0cea68631be80589c5d4272d17bd060e1ca85176
JadeKim042386 <jk042386@gmail.com>
2024-03-18 21:43:08 +0900

    refactor: PasswordGenerator의 길이, 모든 문자 변수를 static 변수로 지정

--- src/main/java/joo/project/my3dbackend/security/PasswordGenerator.java
+++ src/main/java/joo/project/my3dbackend/security/PasswordGenerator.java
@@ -11,15 +11,17 @@ public class PasswordGenerator {
     private static final String CHAR_LOWER = "abcdefghijklmnopqrstuvwxyz";
     private static final String CHAR_UPPER = CHAR_LOWER.toUpperCase();
     private static final String NUMBER = "0123456789";
-    private static final String SPECIAL_CHAR = "!@#$%&*()_+-=[]|,./?><";
+    private static final String SPECIAL_CHAR = "!@#$%&";
+    private static final String ALL_CHARACTERS = CHAR_LOWER + CHAR_UPPER + NUMBER + SPECIAL_CHAR;
+    private static final int MIN_LENGTH = 8;
+    private static final int MAX_LENGTH = 20;
     private static final SecureRandom RANDOM = new SecureRandom();
 
     public String generateRandomPassword() {
-        int minLength = 8, maxLength = 20;
-        int length = RANDOM.nextInt((maxLength - minLength) + 1) + minLength;
+        int length = RANDOM.nextInt((MAX_LENGTH - MIN_LENGTH) + 1) + MIN_LENGTH;
         ArrayList<Character> passwordCharacters = new ArrayList<>();
         // 영문자, 숫자, 특수문자를 각각 2개씩 추가 (최소 길이를 가지는 비밀번호를 기본으로 생성)
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < MIN_LENGTH/4; i++) {
             passwordCharacters.add(CHAR_LOWER.charAt(RANDOM.nextInt(CHAR_LOWER.length())));
             passwordCharacters.add(CHAR_UPPER.charAt(RANDOM.nextInt(CHAR_UPPER.length())));
             passwordCharacters.add(NUMBER.charAt(RANDOM.nextInt(NUMBER.length())));
@@ -27,9 +29,8 @@ public class PasswordGenerator {
         }
 
         // 나머지 글자를 무작위로 추가
-        String allCharacter = CHAR_LOWER + CHAR_UPPER + NUMBER + SPECIAL_CHAR;
-        for (int i = 0; i < length - minLength; i++) {
-            passwordCharacters.add(allCharacter.charAt(RANDOM.nextInt(allCharacter.length())));
+        for (int i = 0; i < length - MIN_LENGTH; i++) {
+            passwordCharacters.add(ALL_CHARACTERS.charAt(RANDOM.nextInt(ALL_CHARACTERS.length())));
         }
         // 무작위로 섞음
         Collections.shuffle(passwordCharacters, RANDOM);
