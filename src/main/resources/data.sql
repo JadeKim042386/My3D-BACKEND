670fbe1095962f3341c29ca3815aa1ea2fd3ecd9
JadeKim042386 <jk042386@gmail.com>
2024-03-22 16:25:26 +0900

    chore: 테스트 데이터 추가 및 구독 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,10 @@ insert into user_account(id, company_id, email, password, nickname, phone, addre
 (nextval('seq_user_account'), null, 'admin@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'admin', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'USER', now(), null),
 (nextval('seq_user_account'), 1, 'adminCompany@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'adminCompany', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'COMPANY', now(), null);
 
+insert into subscribe(id, user_account_id, package_type, subscribe_status, started_at) values
+(nextval('seq_subscribe'), 1, 'FREE', 'STOP', now()),
+(nextval('seq_subscribe'), 2, 'FREE', 'STOP', now());
+
 insert into article_file (id, byte_size, original_file_name, file_name, file_extension, dimension_option) values
 (nextval('seq_article_file'), 1000, 'test.stl', 'test.stl', 'stl', '{"name": "option", "dimensions": [{"name": "dimName", "value": 10.0, "unit": "MM"}]}');
 
