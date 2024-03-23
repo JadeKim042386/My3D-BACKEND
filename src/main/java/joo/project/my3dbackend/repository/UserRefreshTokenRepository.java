e7d9ee529802ecdf9bde858606edb1c84e4ca29d
JadeKim042386 <jk042386@gmail.com>
2024-03-23 19:54:53 +0900

    style: UserRefreshTokenRepository의 쿼리 메소드에 @Param을 사용하여 가독성 개선

--- src/main/java/joo/project/my3dbackend/repository/UserRefreshTokenRepository.java
+++ src/main/java/joo/project/my3dbackend/repository/UserRefreshTokenRepository.java
@@ -3,10 +3,11 @@ package joo.project.my3dbackend.repository;
 import joo.project.my3dbackend.domain.UserRefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
-    @Query("select ua.userRefreshToken from UserAccount ua where ua.id = ?1 and ua.userRefreshToken.reissueCount < ?2")
-    Optional<UserRefreshToken> findByUserAccountIdAndReissueCountLessThan(long id, long count);
+    @Query("select ua.userRefreshToken from UserAccount ua where ua.id = :id and ua.userRefreshToken.reissueCount < :count")
+    Optional<UserRefreshToken> findByUserAccountIdAndReissueCountLessThan(@Param("id") long id, @Param("count") long count);
 }
