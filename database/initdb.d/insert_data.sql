9b3583e82e58d824a8a3dfe27be308081162f87b
JadeKim042386 <jk042386@gmail.com>
2024-03-23 19:46:34 +0900

    chore: RefreshToken 만료 시간과 테이블 추가 및 테스트 데이터 추가

--- database/initdb.d/insert_data.sql
+++ database/initdb.d/insert_data.sql
@@ -1,9 +1,13 @@
 insert into company(id, company_name, homepage) values
 (nextval('seq_company'), 'my3d', null);
 
-insert into user_account(id, company_id, email, password, nickname, phone, address, user_role, created_at, modified_at) values
-(nextval('seq_user_account'), null, 'admin@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'admin', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'USER', now(), null),
-(nextval('seq_user_account'), 1, 'adminCompany@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'adminCompany', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'COMPANY', now(), null);
+insert into user_refresh_token (id, refresh_token, reissue_count) values
+(nextval('seq_user_refresh_token'), 'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDUwNTkzNjAsImV4cCI6MTczNjU5OTM2MH0.Zz-gxdujoo71RZvV59zImALwFY-_kzKTZlwOAoJgIqQ', 0),
+(nextval('seq_user_refresh_token'), 'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDUwNTkzNjAsImV4cCI6MTczNjU5OTM2MH0.Zz-gxdujoo71RZvV59zImALwFY-_kzKTZlwOAoJgIqQ', 0);
+
+insert into user_account(id, company_id, user_refresh_token_id, email, password, nickname, phone, address, user_role, created_at, modified_at) values
+(nextval('seq_user_account'), null, 1, 'admin@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'admin', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'USER', now(), null),
+(nextval('seq_user_account'), 1, 2, 'adminCompany@gmail.com', '$2a$10$QLK9CfBaRvQmAJwqu/EUoOltJxLYad8hQp/BZ4J9.FZZYw/lzBv.O', 'adminCompany', '01011112222', '{"zipcode":"12345", "street":"street", "detail":"detail"}', 'COMPANY', now(), null);
 
 insert into subscribe(id, user_account_id, package_type, subscribe_status, started_at) values
 (nextval('seq_subscribe'), 1, 'FREE', 'STOP', now()),
