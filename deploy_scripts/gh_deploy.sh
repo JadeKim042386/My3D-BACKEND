9ae7562c4f45e33a2200af319d94bd8b11ef5f93
JadeKim042386 <jk042386@gmail.com>
2024-03-24 16:48:23 +0900

    chore: aws 배포 설정

---
+++ deploy_scripts/gh_deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PROJECT_NAME="my3d_backend"
+JAR_PATH="/home/ubuntu/my3d_backend/build/libs/*.jar"
+DEPLOY_PATH="/home/ubuntu/$PROJECT_NAME/"
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+BUILD_JAR=$(ls $JAR_PATH)
+JAR_NAME=$(basename $BUILD_JAR)
+STATIC_PATH="/home/ubuntu/$PROJECT_NAME/src/main/resources/static"
+
+echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
+
+echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
+echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
+cp $BUILD_JAR $DEPLOY_PATH
+
+# -- Deploy
+IDLE_PROFILE="prod"
+IDLE_APPLICATION_PATH=$DEPLOY_PATH$JAR_NAME
+
+echo "> $IDLE_PROFILE 배포" >> $DEPLOY_LOG_PATH
+nohup java -jar -DSpring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+
+echo "> static 경로 저장" >> $DEPLOY_LOG_PATH
+echo "set \$static_path ${STATIC_PATH};" |sudo tee /etc/nginx/conf.d/static-path.inc
+
+sleep 3
+
+echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
