9d6d856719f25d093b7f09010dff7278303ddc5c
JadeKim042386 <jk042386@gmail.com>
2024-03-25 12:51:02 +0900

    chore: 이미 배포된 application을 종료하는 스크립트 추가

--- deploy_scripts/gh_deploy.sh
+++ deploy_scripts/gh_deploy.sh
@@ -5,7 +5,7 @@ DEPLOY_PATH="/home/ubuntu/$PROJECT_NAME/"
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
-BUILD_JAR=$(ls $JAR_PATH)
+BUILD_JAR=$(ls /home/ubuntu/my3d_backend/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 STATIC_PATH="/home/ubuntu/$PROJECT_NAME/src/main/resources/static"
 
@@ -14,6 +14,19 @@ echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
+
+# -- Current Application Kill
+echo "> $CURRENT_PROFILE 에서 구동중인 애플리케이션 pid 확인" >> $DEPLOY_LOG_PATH
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $DEPLOY_LOG_PATH
+else
+  echo "> sudo kill -15 $CURRENT_PID" >> $DEPLOY_LOG_PATH
+  sudo kill -15 $CURRENT_PID
+  sleep 5
+fi
 
 # -- Deploy
 IDLE_PROFILE="prod"
